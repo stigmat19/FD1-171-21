b81d61148b6c7b3d75ca65227d426e4e3182c73c
stigmat19@yandex.ru <stigmat19@yandex.ru>
2022-02-03 20:29:49 +0300

    make menu scroll and humburger

---
+++ tea/js/script.js
@@ -0,0 +1,69 @@
+var imgUrlInput = document.getElementById("imgUrl");
+var titleInput = document.getElementById("title");
+var titleSmallInput = document.getElementById("titleSmall");
+var descrInput = document.getElementById("descr");
+var weightInput = document.getElementById("weight");
+var priceInput = document.getElementById("price");
+var ratingInput = document.getElementById("rating");
+var cardWrap = document.getElementById("card-wrap");
+var card = [];
+
+//function
+function getData (){
+	var obj = {};
+	obj.rating = ratingInput.value;
+	obj.imgUrl = imgUrlInput.value;
+	obj.title = titleInput.value;
+	obj.titleSmall = titleSmallInput.value;
+	obj.descr = descrInput.value;
+	obj.weight = weightInput.value;
+	obj.price = priceInput.value;
+	card.push(obj);
+	console.log(card);
+	render();
+}
+
+
+
+function render (){
+
+	var str = "";
+	for (let i = 0; i < card.length; i++) {
+		var item = card[i];
+		str += `<div class="card">
+                    <div class="card-top">
+                        <span>${item.rating}</span>
+                        <img src="img/heart.svg" alt="">
+                    </div>
+                    <div>
+                        <img src="${item.imgUrl}" alt="">
+                    </div>
+                    <h4>${item.title}</h4>
+                    <h5>${item.titleSmall}</h5>
+                    <p class="descr">${item.descr}</p>
+                    <p class="price">${item.weight} / <span>${item.price}</span></p>
+                    <button>В корзину</button>
+                </div>`;
+	}
+	cardWrap.innerHTML = str;
+
+
+
+	if(gender) {
+		if(age > 63){
+			pensiya
+		}
+		else{
+			ne pensiya
+		}
+	}
+	else{
+		if(age > 58){
+			pensiya
+		}
+		else{
+			ne pensiya
+		}
+	}
+
+}
